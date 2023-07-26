79a5cc991223e06f022a160c6241eb7d59f0b83d
Thalisson Vieira <32040335+Thalinhos@users.noreply.github.com>
2023-07-26 16:54:24 -0300

    Commit 1
    Já refiz isso umas 3x meu deus.
    Tá, aqui eu to basicamente testando o babel pra transpilar js em uma pasta dist, que é completamente desnecessário aqui, mas eu queria testar de qualquer forma.
    E tbm, aproveitar pra testar deploy usando node e express. Poderia simplesmente só servir o html, mas usei o express pra testar plataformas e boas práticas de deploy.

---
+++ blog/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
+
+app.listen(3000, () => {
+    console.log('listening on port http://localhost:3000');
+})
